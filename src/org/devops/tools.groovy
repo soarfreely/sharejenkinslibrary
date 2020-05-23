32eafe739a162b141a3c5fa94bc5ba0c9dd82627
zhangxiang <zhangxiang_php@vchangyi.com>
2020-05-23 21:20:53 +0800

    格式化输出

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,6 +1,15 @@
 package org.devops
 
-// 打印输出
-def printMsg(content) {
-  println(content)
+// 格式化输出
+def printMsg(content,color) {
+    colors = [
+            'red'   : "\033[40;31m >>>>>>>>>>>${content}<<<<<<<<<<< \033[0m",
+            'blue'  : "\033[47;34m ${content} \033[0m",
+            'green' : "[1;32m>>>>>>>>>>${content}>>>>>>>>>>[m",
+            'green1' : "\033[40;32m >>>>>>>>>>>${content}<<<<<<<<<<< \033[0m"
+        ]
+
+        ansiColor('xterm') {
+        println(colors[color])
+    }
 }
