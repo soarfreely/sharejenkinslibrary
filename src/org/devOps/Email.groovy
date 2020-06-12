6ad7142e870beacb637fc6d232c7b837e3413dc3
zhangxiang <zhangxiang_php@vchangyi.com>
2020-06-12 16:08:47 +0800

    发送邮件

---
+++ src/org/devOps/Email.groovy
@@ -0,0 +1,38 @@
+package org.devOps
+
+//邮件内容
+def Email(status, toEmail){
+    emailext body: """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body leftmargin="8" marginwidth="0" topmargin="8" marginheight="4" offset="0">
+            <h3>追求质量无极限，强化安全至永远</h3>
+            <h2>尊重生产</h2>
+            <table width="95%" cellpadding="0" cellspacing="0" style="font-size: 11pt; font-family: Tahoma, Arial, Helvetica, sans-serif">
+                <tr>
+                    <td><br />
+                        <b><font color="#0B610B">构建信息</font></b>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>项目名称：${JOB_NAME}</li>
+                            <li>构建编号：${BUILD_ID}</li>
+                            <li>构建状态: ${status} </li>
+                            <li>项目地址：<a href="${BUILD_URL}">${BUILD_URL}</a></li>
+                            <li>构建日志：<a href="${BUILD_URL}console">${BUILD_URL}console</a></li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+            </table>
+            </body>
+            </html>  """,
+            subject: "Jenkins-${JOB_NAME}项目构建信息 ",
+            to: toEmail
+        
+}
