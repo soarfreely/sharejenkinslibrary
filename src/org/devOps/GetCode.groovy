40a14d64408459aff524fa4fc66dad3b5185e26b
zhangxiang <zhangxiang_php@vchangyi.com>
2020-06-12 16:17:52 +0800

    拉取代码

---
+++ src/org/devOps/GetCode.groovy
@@ -0,0 +1,20 @@
+package org.devOps
+
+//代码检出
+def GetCode(srcUrl, tagName, branchName, credentialsId) {
+    //delete 'origin/'
+    if (branchName.startsWith('origin/')){
+        branchName=branchName.minus("origin/")
+    }
+
+    if(tagName == "null"){
+        pathName = "*/${branchName}"
+    }else{
+        pathName = "refs/tags/${tagName}"
+    }
+    checkout([$class: 'GitSCM', branches: [[name: "${pathName}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [], submoduleCfg: [],
+        userRemoteConfigs: [[credentialsId: "${credentialsId}",
+        url: "${srcUrl}"]]])
+}
