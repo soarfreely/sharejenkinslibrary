01cf5b69f8a1ef839adf862f74532b70c3191eea
zhangxiang <zhangxiang_php@vchangyi.com>
2020-06-17 19:24:30 +0800

    debug7

--- vars/callableDemo.groovy
+++ vars/callableDemo.groovy
@@ -1,19 +1,19 @@
 // 回调demo
-def call(name1, name2) {
-    println('1111111');
-}
+// def call(name1, name2) {
+//     println('1111111');
+// }
 
-def call2(Closure callableBody) {
+def call(Closure callableBody) {
     println('1111111');
 //     println(run_composer);
 //     println(php_project_path);
     println('2222222');
-    node('callableDemo') {
-        callableBody();
-        println('回调方法1');
-        println(run_composer);
-        println(php_project_path);
-    }
+
+    callableBody();
+    println('33333333333');
+    println(run_composer);
+    println(php_project_path);
+
 //     callableBody();
 //     println('回调方法');
 //     println(run_composer);
