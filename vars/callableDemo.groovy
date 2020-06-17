0ab4cf1ded657d40096146c7abee7e84fafe46d4
zhangxiang <zhangxiang_php@vchangyi.com>
2020-06-17 18:43:20 +0800

    回调调试2

--- vars/callableDemo.groovy
+++ vars/callableDemo.groovy
@@ -1,6 +1,6 @@
 // 回调demo
-def call(Closure callableDemo) {
-    callableDemo();
+def call(Closure callableBody) {
+    callableBody();
     println('回调方法');
     println(run_composer);
     println(php_project_path);
