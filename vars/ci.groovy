c3b8c59654ff9987a533a323f6c6c31563282db8
zhangxiang <zhangxiang_php@vchangyi.com>
2020-06-18 13:29:20 +0800

    工具

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,8 +1,7 @@
 def call(Closure body) {
-     aa = body(bb, cc)
+     body()
 
      def tool = new org.devOps.Tools()
      tool.printMsg("my lib", 'green')
 
-     tool.printMsg("${bb}", 'green')
-}
+     tool.printMsg(body.run_composer, 'green')
