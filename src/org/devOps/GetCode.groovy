941d1feee2990c2ab0987da0069cba089f155679
zhangxiang <zhangxiang_php@vchangyi.com>
2020-06-19 14:51:00 +0800

    debug1

--- src/org/devOps/GetCode.groovy
+++ src/org/devOps/GetCode.groovy
@@ -3,15 +3,15 @@ package org.devOps
 //代码检出
 def GetCode(srcUrl, credentialsId, branchName, tagName = null) {
     //delete 'origin/'
-    if (branchName.startsWith('origin/')){
-        branchName=branchName.minus("origin/")
-    }
-
-    if(tagName == "null"){
-        pathName = "*/${branchName}"
-    }else{
-        pathName = "refs/tags/${tagName}"
-    }
+//     if (branchName.startsWith('origin/')){
+//         branchName=branchName.minus("origin/")
+//     }
+//
+//     if(tagName == "null"){
+//         pathName = "*/${branchName}"
+//     }else{
+//         pathName = "refs/tags/${tagName}"
+//     }
 
     println("-------------")
     println("${branchName}")
