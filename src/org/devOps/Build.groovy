739d3bd455e4c38eb435dea22f54e7e4c7a32fb1
zhangxiang <zhangxiang_php@vchangyi.com>
2020-06-20 10:04:52 +0800

    8

--- src/org/devOps/Build.groovy
+++ src/org/devOps/Build.groovy
@@ -38,6 +38,8 @@ def tar(projectName, targetIp, credentialsId) {
             ssh -o StrictHostKeyChecking=no -l root 172.17.0.3 uname -a
             echo 123
             ls -al
+            ssh -o StrictHostKeyChecking=no -l root 172.17.0.3 uname -a && pwd
+            ls -al
             scp ${tarName} ${targetIp}:${dir}
             echo 456
             ssh root@${targetIp} -tt "ls -al && cd ${dir} && tar zxvf ${projectName} -C ${projectName}"
