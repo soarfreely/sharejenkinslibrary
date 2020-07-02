56fc9ad56d7977c15b3aef4d2c337a82da3f39ca
zhangxiang <zhangxiang_php@vchangyi.com>
2020-07-02 16:48:44 +0800

    debug

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,7 +1,4 @@
 def call(Closure body) {
-     println("JOB_NAME:${JOB_NAME}")
-     println("WORKSPACE:${WORKSPACE}")
-     println("JENKINS_HOME:${JENKINS_HOME}")
      body()
 
      def tool = new org.devOps.Tools()
@@ -42,13 +39,17 @@ def call(Closure body) {
      tool.printMsg(body.repository, 'green')
 
     // jenkins 工作目录
-    String workspace = "/home/soar/app/nginx-php-fpm/www/jenkins/workspace"
-    println(workspace)
+//     String workspace = "/home/soar/app/nginx-php-fpm/www/jenkins/workspace"
+//     println(workspace)
     pipeline {
+        println("JOB_NAME:${JOB_NAME}")
+        println("WORKSPACE:${WORKSPACE}")
+        println("JENKINS_HOME:${JENKINS_HOME}")
+
     	agent {
     		node {
     			label "master" // 指定运行节点的标签或者名称
-    			customWorkspace "projectName/${workspace}" // 指定运行工作目录（可选）
+    			customWorkspace "projectName/${WORKSPACE}" // 指定运行工作目录（可选）
     		}
     	}
 
