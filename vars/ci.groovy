b5d641c2bf25ae78981423d18b19b2c1a03993bb
zhangxiang <zhangxiang_php@vchangyi.com>
2020-07-02 18:34:42 +0800

    debug

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -45,7 +45,7 @@ def call(Closure body) {
     	agent {
     		node {
     			label "master" // 指定运行节点的标签或者名称
-    			customWorkspace "${customWorkspace}" // 指定运行工作目录（可选）
+//     			customWorkspace "${customWorkspace}" // 指定运行工作目录（可选）
     		}
     	}
         // 指定运行选项（可选）
