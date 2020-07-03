ddb77c5219c26dcd0a21933d7ef20a9b548a438d
zhangxiang <zhangxiang_php@vchangyi.com>
2020-07-03 17:54:06 +0800

    授权发布

--- vars/idc.groovy
+++ vars/idc.groovy
@@ -59,19 +59,23 @@ def call(Closure body) {
     	        if ('prod' == branch) {
                     steps {
                         timeout(time:5, unit:"MINUTES") {
-                            input {
-                                message "Should we continue ?"
-                                ok "Yes, we should."
-                                submitter "gavin, admin" // 指定允许提交的用户
+                            input (
+                                message: "Should we continue ?"
+                                ok: "Yes, we should."
+                                submitter: "gavin, admin" // 指定允许提交的用户
                                 parameters {
                                     string(name: 'who', defaultValue: 'gavin', description: 'Who are you?')
                                 }
-                            }
+                            )
 
                             if (submitter.contains("${who}") {
                                 script {
                                     tool.printMsg("${who},同意发布", 'green')
                                 }
+                            } else {
+                                tool.printMsg("${who},同意发布", 'red')
+                                throw new RuntimeException("组长拒绝部署")
+                                false
                             }
                         }
                     }
